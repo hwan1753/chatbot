84fe39b9536d68ed2c25a0147de4bc79472ddbf6
sai bhaskar <35323355+saibhaskar24@users.noreply.github.com>
2019-05-24 07:49:32 +0530

    Chatbot with dialog flow integration

--- app/src/main/java/com/example/chatbot/MainActivity.java
+++ app/src/main/java/com/example/chatbot/MainActivity.java
@@ -1,16 +1,24 @@
 package com.example.chatbot;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
+import android.speech.tts.TextToSpeech;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.JsonElement;
 
+import java.util.Locale;
 import java.util.Map;
 
 import ai.api.AIDataService;
@@ -26,10 +34,14 @@ import ai.api.model.Result;
 
 
 
-public class MainActivity extends AppCompatActivity  implements AIListener {
+public class MainActivity extends AppCompatActivity  implements AIListener, TextToSpeech.OnInitListener {
 
-    private Button listenButton,send;
+    private Button send;
+    ImageButton listenButton;
     private TextView resultTextView;
+    private final int MY_PERMISSIONS_RECORD_AUDIO = 1;
+    TextToSpeech textToSpeech;
+    boolean granted = false;
     EditText mess;
     private AIRequest aiRequest;
     private AIService aiService;
@@ -39,17 +51,20 @@ public class MainActivity extends AppCompatActivity  implements AIListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        while (!granted) {
+            requestAudioPermissions();
+        }
 
         final AIConfiguration config = new AIConfiguration("9a8fa0380cc343d7af56217ac01ceb75",
                 AIConfiguration.SupportedLanguages.English,
                 AIConfiguration.RecognitionEngine.System);
-
+        textToSpeech = new TextToSpeech(this,  this);
 
         aiService = AIService.getService(this, config);
         aiService.setListener(this);
         aiDataService = new AIDataService(config);
         aiRequest = new AIRequest();
-        listenButton = (Button) findViewById(R.id.listenButton);
+        listenButton = (ImageButton) findViewById(R.id.listenButton);
         resultTextView = (TextView) findViewById(R.id.resultTextView);
         send = (Button)findViewById(R.id.send);
         mess = (EditText)findViewById(R.id.mess);
@@ -72,6 +87,7 @@ public class MainActivity extends AppCompatActivity  implements AIListener {
                 "\nAction: " + result.getAction() +
                 "\nParameters: " + parameterString
         + "\nTO speech " + result.getFulfillment().getSpeech());
+        speakOut(result.getFulfillment().getSpeech());
         listenButton.setEnabled(true);
 
     }
@@ -123,5 +139,77 @@ public class MainActivity extends AppCompatActivity  implements AIListener {
             }
         }.execute(aiRequest);
         mess.setText("");
+    }
+
+
+    private void requestAudioPermissions() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            //When permission is not granted by user, show them message why this permission is needed.
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.RECORD_AUDIO)) {
+                Toast.makeText(this, "Please grant permissions to record audio", Toast.LENGTH_LONG).show();
+
+                //Give user option to still opt-in the permissions
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.RECORD_AUDIO},
+                        MY_PERMISSIONS_RECORD_AUDIO);
+
+            } else {
+                // Show user dialog to grant permission to record audio
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.RECORD_AUDIO},
+                        MY_PERMISSIONS_RECORD_AUDIO);
+            }
+        }
+        //If permission is granted, then go ahead recording audio
+        else if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO)
+                == PackageManager.PERMISSION_GRANTED) {
+
+            granted = true;
+        }
+    }
+
+    /**
+     * Called to signal the completion of the TextToSpeech engine initialization.
+     *
+     *
+     *
+     */
+
+    @Override
+    protected void onDestroy() {
+        if (textToSpeech != null) {
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+        super.onDestroy();
+        super.onDestroy();
+    }
+
+    private void speakOut(String out) {
+        textToSpeech.speak(out, TextToSpeech.QUEUE_ADD, null);
+
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+
+            int result = textToSpeech.setLanguage(Locale.ENGLISH);
+
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "This Language is not supported");
+            } else {
+                speakOut("Permission Granted");
+            }
+
+        } else {
+            Log.e("TTS", "Initilization Failed!");
+        }
     }
 }
